4d76619af0f1615b7ae68a6375e95e8cda700d8f
kjlee <kjlee.ic@gmail.com>
2017-03-24 20:29:33 +0900

    HW2

---
+++ D05_uart_apb/rtl_sim/tb_uart_rx.f
@@ -0,0 +1,5 @@
+./../verilog/cmsdk_apb_uart.v
+./model/cmsdk_clkreset.v
+./model/sg_uart_tx.v
+./model/sg_uart_rx.v
+./tb/tb_uart_rx.v
---
+++ D05_uart_apb/rtl_sim/tb_uart_tx.f
@@ -0,0 +1,4 @@
+./../verilog/cmsdk_apb_uart.v
+./model/cmsdk_clkreset.v
+./model/sg_uart_tx.v
+./tb/tb_uart_tx.v
